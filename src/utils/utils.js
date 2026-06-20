a8fada291c8a0efa37ad1d1c18e920d5518003d6
codedpool <codedpool10@gmail.com>
2026-06-21 02:22:54 +0530

    remove unused helper

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,7 +4,3 @@ export function respondJSON(res, status, payload) {
   res.writeHead(status, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(payload));
 }
-
-export function boolToFloat(ok) {
-  return ok ? 1 : 0;
-}
